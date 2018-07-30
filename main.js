260cb20296140fc5fa9ab03d1e84d9eeb67353c9
Helen <helen@Helens-MacBook-Pro.local>
2018-07-29 20:11:58 -0500

    worked on Mod Squad

--- main.js
+++ main.js
@@ -15,40 +15,135 @@ for (let y = 1; y < cookies.length; y++) {
 //Conjunction Function (this one works)
 
 
-const conjunction = function (firstWord, secondWord) {
+let conjunction = function (firstWord, secondWord) {
     const conjoinedWord = `${firstWord} ${secondWord}`;
+    // I moved this console.log inside the function as conjoinedWord can only be access from within the function.  
     console.log(conjoinedWord);
+
+    // I then defined what the function is returning.
+    return conjoinedWord;
 }
 
-conjunction("Master", "Card");
+// I stored the function's value when 2 words are passed through the function to a variable that can be accessed outside of the function.  
+let newWord = conjunction("Master", "Card");
+
+// accessing the word created by the function outside of the function.  
+console.log(newWord);
+
+
+
 
 
 //Mod Squad
-{
-    const ModSquad = {
-        members: ["Pete Cochran", "Linc Hayes", "Julie Barnes", "Capt. Adam Greer", "Chief Barney Metcalf"],
+// {
+//     const ModSquad = {
+//         members: ["Pete Cochran", "Linc Hayes", "Julie Barnes", "Capt. Adam Greer", "Chief Barney Metcalf"],
 
         
+//         series: {
+//             start: "1968",
+//             end: "1973"
+//         }
+//     }
+
+//     let HTMLRepresentation = `<h1>The Mod Squad</h1>`
+//     console.log(HTMLRepresentation);
+
+//     ModSquad.members.forEach(person => {
+//         //console.log(person);
+//         HTMLRepresentation += `<div>${ModSquad.members}</div>`;
+//         //console.log(HTMLRepresentation);
+        
+//     }) 
+
+    
+//    document.querySelector(".show-info").innerHTML = HTMLRepresentation;
+        
+// }
+
+
+// ------Practice from one of the scope videos given --
+// QUESTION - this function was given in video #3 but does not actually work - why is that?
+
+const createPrinter = () => {
+    const name = "King";
+    // return name; 
+
+    const printName = () => {
+        console.log(name); 
+    }
+    
+   return printName;
+}
+const myPrinter = createPrinter();
+console.log(myPrinter);
+
+
+
+
+
+
+
+// Mod Squad --------------------------------------
+
+//This variable was replaced outside of the curly-brackets, making it a global scope, so that it could be accessed on the global level.
+let HTMLRepresentation = `<h1>The Mod Squad</h1>`
+{
+    const ModSquad = {
+        "members": ["Pete Cochran", "Linc Hayes", "Julie Barnes", "Capt. Adam Greer", "Chief Barney Metcalf"],
         series: {
             start: "1968",
             end: "1973"
         }
     }
-
-    let HTMLRepresentation = `<h1>The Mod Squad</h1>`
-    console.log(HTMLRepresentation);
-
-    ModSquad.members.forEach(person => {
-        //console.log(person);
-        HTMLRepresentation += `<div>${ModSquad.members}</div>`;
-        //console.log(HTMLRepresentation);
-        
-    }) 
-
+    console.log(ModSquad);
     
-   // document.querySelector(".show-info").innerHTML = HTMLRepresentation;
+
+    ModSquad.members.forEach(member => {
+        HTMLRepresentation += `<div>${member}</div>`;
         
+        //I consol logged the new value of HTMLRepresentationt to make sure it held the value we were looking for 
+        console.log(HTMLRepresentation);
+        
+    })
+    //I then console logged the same variable to make sure it held the expected value.
+    console.log(HTMLRepresentation);
+       
 }
+//printed the variable in the correct section in HTML and ultimately to the page.
+document.querySelector(".show-info").innerHTML = HTMLRepresentation;
+
+
+
+
+
+// -------SIMON SAYS ----------------------------------
+// There are two separate scoping issues in this code. Remember, for and if are their own block scopes and if something is declared with let or const then that variable only exists in that scope.
+
+
+
+const locations = [[1,1], [1,2], [1,3], [2,1], [2,2], [2,3]]
+
+for (let k = 0; k < locations.length; k++) {
+    const currentLocation = locations[k]
+
+    if (currentLocation[0] > 2) {
+        const invalidLocation = true
+        console.log("This location is invalid")
+    }
+}
+
+/*
+    Since k is used as the loop counter, it will be the
+    total number of locations, not the invalid ones. You
+    need to fix this somehow.
+*/
+console.log(`There were ${k} invalid locations`)
+
+
+//-----------------------------------------------------
+        //    TreeHouse Practice ------
+
 
 
 //TreeHouse Exercises practicing For Each loops
@@ -96,50 +191,3 @@ conjunction("Master", "Card");
 //     console.log(dayAbbreviations);
 // });
 
-//Simon Says
-
-const locations = [[1,1], [1,2], [1,3], [2,1], [2,2], [2,3]]
-let invalLocalCounter = 0;
-
-for (let k = 0; k < locations.length; k++) {
-    const currentLocation = locations[k]
-
-    if (currentLocation[1] > 2) {
-        let invalidLocation = true;
-        
-        if (invalidLocation === true) {
-            console.log("This location is invalid")
-            invalLocalCounter += 1;
-            console.log(invalLocalCounter);
-        }
-
-    }
-
-    
-}
-
-/*
-    Since k is used as the loop counter, it will be the
-    total number of locations, not the invalid ones. You
-    need to fix this somehow.
-*/
-console.log(`There were ${invalLocalCounter} invalid locations`)
-
-//Advanced Challenge: Lambda Llama
-
-const llamaNamer = function () {
-    const possibleNames = ["Larry", "Leon", "Leona", "Les", "Laura", "Lemony", "Lars", "Lekisha"]
-    const randomizer = Math.floor(Math.random() * 7)
-
-    const namer = function () {
-        const suffix = " the Llama"
-        const name = possibleNames[randomizer]
-        return name + suffix;
-    }
-
-    return namer
-
-    
-}
-    nameMaker = llamaNamer()
-    console.log(nameMaker())
